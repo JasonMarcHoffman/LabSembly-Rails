8789eb2e3af56bfc784a094bd3e48aea5e86a64b
Nikola Zatezalo <tesla.zatezalo@gmail.com>
2020-02-13 14:00:41 +0200

    Feat: added seed to create 3 labs and 3 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Lab.create(name: "First Lab", location: "braam")
+Lab.create(name: "Second Lab", location: "Sandton")
+Lab.create(name: "Third Lab", location: "Morning Side")
+
+User.create(first_name: "SuperAdmin", last_name: "User", type: "SuperAdmin", lab_id: 1, email: "superadmin@User.com", password: "123123")
+User.create(first_name: "Manager", last_name: "User", type: "Manager", lab_id: 1, email: "Manager@User.com", password: "123123")
+User.create(first_name: "Normal", last_name: "User", type: "Normal", lab_id: 1, email: "Normal@User.com", password: "123123")
